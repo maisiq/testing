c2ba5662cba520e6e531bd3a0564b686047fd40d
maisiq <vyruscd@gmail.com>
2023-12-04 19:22:27 +0400

    Add cpu-bound task

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import asyncio
 
 
+async def cpu_bound():
+    c = 0
+    for _ in range(1000000):
+        c += 1
+    return c
+
 
 async def main():
     task1 = asyncio.create_task(asyncio.sleep(3))
+    task2 = asyncio.create_task(cpu_bound())
 
-    r, _ = await asyncio.wait([task1])
+    r, _ = await asyncio.wait([task1, task2])
+
     print(
         [r.result() for r in r]
         )
